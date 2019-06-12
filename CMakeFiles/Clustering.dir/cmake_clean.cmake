ba91baec312809f01d173a961e8109907e843682
Pedro Reis <pedrohsareis@gmail.com>
2019-06-12 00:50:54 -0300

    Código funcional de floodfill

---
+++ CMakeFiles/Clustering.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Clustering.dir/main.cpp.o"
+  "Clustering.pdb"
+  "Clustering"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Clustering.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
